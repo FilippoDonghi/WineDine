5166cf21b62c229c09050d78a57a24855786526e
valeefuso <156070058+valeefuso@users.noreply.github.com>
2025-03-06 13:11:03 +0100

    aggiornamento card visualize

--- app/src/main/java/it/unimib/winedine/ui/home/fragment/BottleVisualizeFragment.java
+++ app/src/main/java/it/unimib/winedine/ui/home/fragment/BottleVisualizeFragment.java
@@ -4,6 +4,7 @@ import static it.unimib.winedine.util.Constants.BUNDLE_KEY_CURRENT_BOTTLE;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,7 +41,29 @@ public class BottleVisualizeFragment extends Fragment {
 
         ((TextView) view.findViewById(R.id.textViewTitle)).setText(currentBottle.getTitle());
         ((TextView) view.findViewById(R.id.textViewDescription)).setText(currentBottle.getDescription());
-        ((TextView) view.findViewById(R.id.textViewAverageRating)).setText(currentBottle.getAverageRating());
+        TextView ratingView = view.findViewById(R.id.textViewAverageRating);
+        String originalRating = currentBottle.getAverageRating();
+
+        try {
+
+            // 2. Troncamento a 4 caratteri
+            String truncatedRating = originalRating.length() > 4 ?
+                    originalRating.substring(0, 4) :
+                    originalRating;
+
+            // 3. Conversione e scaling
+            double ratingValue = Double.parseDouble(truncatedRating);
+            double scaledRating = ratingValue * 5;
+
+            // 4. Formattazione con 1 decimale
+            String formattedRating = String.format("%.1f ★", scaledRating);
+            ratingView.setText(formattedRating);
+
+        } catch (NumberFormatException e) {
+            // Gestione errori di conversione
+            ratingView.setText("0.0 ★");
+            Log.e("RatingError", "Formato rating non valido: " + originalRating, e);
+        }
         ((TextView) view.findViewById(R.id.textViewRatingCount)).setText(currentBottle.getRatingCount());
         ((TextView) view.findViewById(R.id.textViewScore)).setText(currentBottle.getScore());
         ((TextView) view.findViewById(R.id.textViewPrice)).setText(currentBottle.getPrice());
